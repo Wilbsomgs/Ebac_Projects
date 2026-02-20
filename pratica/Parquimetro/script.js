faac8923fffe601604ca7e27de1c22db2b4467e8
Wilbsom gs <willsuporte@gmail.com>
2026-02-20 06:23:08 -0300

    (wip)

---
+++ pratica/Parquimetro/script.js
@@ -0,0 +1,3 @@
+// Receber valor inserido pelo usuário para calcular o tempo no estacionamento 
+// Calcular e exibir tempo correspondente e o troco
+// Exibir mensagem de "Valor insuficiente" caso o valor seja menor que R$1,00

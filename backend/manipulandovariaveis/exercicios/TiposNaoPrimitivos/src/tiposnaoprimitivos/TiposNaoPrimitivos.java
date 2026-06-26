2087e8578392cbc39b56379bec0ecb832d357911
Wilbsom <willsuporte12@gmail.com>
2026-06-25 21:20:33 -0300

    adiciona Tipos não primitivos

---
+++ backend/manipulandovariaveis/exercicios/TiposNaoPrimitivos/src/tiposnaoprimitivos/TiposNaoPrimitivos.java
@@ -0,0 +1,9 @@
+package tiposnaoprimitivos;
+
+public class TiposNaoPrimitivos {
+    public static void main(String[] args) throws Exception {
+        //Tipos primitivos: int, double, float, char, boolean
+        
+
+    }
+}
